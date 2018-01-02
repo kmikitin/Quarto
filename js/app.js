62aef4ba9c0016f78aabc138882053aa2ff8f0c9
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-02 11:31:14 -0600

    Commit 2 - finished css for now, started pseudocoding the game in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log('Connected!')
+
+/*
+start button will begin the game (run initial functions)
+each game piece div needs an event listener
+on the click the piece will be highlighted (salmon color)
+when the piece is selected it it passed into a function as an argument
+the second argument for that function will be the place on the board that the piece is going to
+
+
+GAME LOGIC:
