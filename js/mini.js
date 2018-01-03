5c68b170ba1e013e8dd4a37d65d302fd0e3d59e1
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-03 14:34:52 -0600

    Commit 18 - added a return in each if/else if statement to exit the loop

--- js/mini.js
+++ js/mini.js
@@ -13,37 +13,34 @@ function selectPiece(evt){
 	};
 };
 
-function choosePlacement(gamePiece, spotOnBoard){
-	gamePiece = currentPiece;
-	$(gamePiece).css('border', '1px solid rgb(0, 0, 0)');
-	$(gamePiece).children().css('background-color', '#F9F8F8');
-	spotOnBoard = this;
-	$(gamePiece).appendTo($(spotOnBoard));
-	$(spotOnBoard).addClass($(gamePiece).attr('class'));
-	// get the ids of the spots currently in place
-	spotsInPlayId.push($(spotOnBoard).attr('id'));
-};
-
 function checkWinner(){
 	for(let i = 0; i < spotsInPlayId.length; i++){
 		// check each of the 4 board configurations that make a win
 		// get the classes of each of the spots being checked
 		// look for matches of one of the six possibilities
 		// one and three
+		// console.log(i)
 		if(spotsInPlayId.includes('one') === true &&  spotsInPlayId.includes('three') === true){
 		// console.log('numbers 1 and 3');
 			if($('#one').attr('class').includes('dark') && $('#three').attr('class').includes('dark')) {
 				console.log('dark win!');
+				// have to return to exit the loop
+				return 'dark win!'
 			} else if ($('#one').attr('class').includes('square') && $('#three').attr('class').includes('square')){
 				console.log('square win!');
+				return 'square win!'
 			} else if ($('#one').attr('class').includes('circle') && $('#three').attr('class').includes('circle')){
 				console.log('circle win!');
+				return 'circle win!'
 			} else if ($('#one').attr('class').includes('light') && $('#three').attr('class').includes('light')){
-				console.log('light win!');	
+				console.log('light win!');
+				return 'light win!'	
 			} else if ($('#one').attr('class').includes('big') && $('#three').attr('class').includes('big')){
 				console.log('big win!');
+				return 'big win!'
 			} else if ($('#one').attr('class').includes('small') && $('#three').attr('class').includes('small')){
 				console.log('small win!');
+				return 'small win!'
 			}
 		}			
 		// three and four
@@ -96,11 +93,21 @@ function checkWinner(){
 			} else if ($('#two').attr('class').includes('small') && $('#four').attr('class').includes('small')){
 				console.log('small win!');
 			}
-		} else {
-		console.log('no winning matches');	
 		}	
 	}
 
+};
+
+function choosePlacement(gamePiece, spotOnBoard){
+	gamePiece = currentPiece;
+	$(gamePiece).css('border', '1px solid rgb(0, 0, 0)');
+	$(gamePiece).children().css('background-color', '#F9F8F8');
+	spotOnBoard = this;
+	$(gamePiece).appendTo($(spotOnBoard));
+	$(spotOnBoard).addClass($(gamePiece).attr('class'));
+	// get the ids of the spots currently in place
+	spotsInPlayId.push($(spotOnBoard).attr('id'));
+	checkWinner();
 };
 
 	// when a win is determined the winning pieces need to be highlighted, maybe a modal to announce it
